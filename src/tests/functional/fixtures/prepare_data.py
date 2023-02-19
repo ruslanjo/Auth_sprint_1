e530d90dc21d25b7786d6ef3ccce2a1fd5bf5922
Ruslan Tarutin <tarutin.ruslan99@gmail.com>
2023-02-19 16:16:51 +0300

    roles tests: [created tests settings]

--- src/tests/functional/fixtures/prepare_data.py
+++ src/tests/functional/fixtures/prepare_data.py
@@ -1,13 +1,7 @@
 import pytest
 
-superuser_id = '189d5aac-92ca-465e-8eda-8220aa7dd162'
-superuser_login = 'test_superuser_1'
-superuser_password = 'password'
-
-role_1_id = '1d355ac2-62b6-44b9-827c-080a0bc40841'
-role_1_name = 'test purposes role 1'
-role_2_id = 'e46c0d12-497b-485d-ac41-37a486c67dab'
-role_2_name = 'test purposes role 2'
+from src.tests.functional.settings.role_settings import (superuser_id, superuser_password, superuser_login,
+                                                         role_1_id, role_2_id, role_1_name, role_2_name)
 
 
 @pytest.fixture(scope='session', autouse=True)
--- src/tests/functional/src/test_roles.py
+++ src/tests/functional/src/test_roles.py
@@ -12,17 +12,6 @@ async def test_get_all_roles(aiohttp_session, make_get_request):
     assert response['status'] == http.HTTPStatus.OK
 
 
-@pytest.mark.asyncio
-async def test_create_role(aiohttp_session, make_post_request):
-    endpoint = '/roles-management/'
-
-    response = await make_post_request(endpoint, body={'name': 'test_role_3'})
-    b = response['body']
-    t = type(b)
-    assert response['body']['name'] == 'test_role_3'
-    assert response['status'] == http.HTTPStatus.CREATED
-
-    # TODO добавить удаление созданной роли
 
 
 
